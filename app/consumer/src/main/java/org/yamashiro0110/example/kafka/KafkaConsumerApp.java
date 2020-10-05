ceab9f7c513f1d31bcd594a33cbf8f1820e64dbc
YamashiroRyota <yamashiro0110@gmail.com>
2020-10-05 22:32:21 +0900

    Update KafkaConsumerApp.java
    add Consumer Factory config

--- app/consumer/src/main/java/org/yamashiro0110/example/kafka/KafkaConsumerApp.java
+++ app/consumer/src/main/java/org/yamashiro0110/example/kafka/KafkaConsumerApp.java
@@ -1,23 +1,52 @@
 package org.yamashiro0110.example.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.Map;
 
 @SpringBootApplication
+@EnableKafka
+@Slf4j
 public class KafkaConsumerApp {
-    private final Logger logger = LoggerFactory.getLogger(KafkaConsumerApp.class);
+    @Value("${kafka.bootstrap-servers:127.0.0.1:9092}")
+    private String bootStrapServers;
 
     public static void main(final String[] args) {
         SpringApplication.run(KafkaConsumerApp.class, args);
     }
 
-    @KafkaListener(topics = "topic-1")
-    public void listen(final ConsumerRecord<?, ?> record) {
-        this.logger.info("messageを取得しました {}", record);
+    @Bean
+    public Map<String, Object> consumerConfig() {
+        return Map.of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootStrapServers,
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class
+        );
     }
 
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(this.consumerConfig());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+        final ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(this.consumerFactory());
+        factory.setConcurrency(3);
+        factory.getContainerProperties().setPollTimeout(5_000);
+        return factory;
+    }
 }
